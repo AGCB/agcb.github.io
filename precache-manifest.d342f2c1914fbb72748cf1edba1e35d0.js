479f961a34fa79bd10dafd0595412e896233e09b
alexClifton <oooalexoooo@gmail.com>
2020-04-18 08:49:20 -0700

    Updates

---
+++ precache-manifest.d342f2c1914fbb72748cf1edba1e35d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c41b2d47852468f2f455f20df6e6f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc2b2ef7b9bdb8c87a2d",
+    "url": "/static/css/main.4e4f938d.chunk.css"
+  },
+  {
+    "revision": "c69238724554de932b27",
+    "url": "/static/js/2.e6381dc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6381dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2b2ef7b9bdb8c87a2d",
+    "url": "/static/js/main.9131b286.chunk.js"
+  },
+  {
+    "revision": "0c3eed15baa5f629721f",
+    "url": "/static/js/runtime-main.874f537c.js"
+  }
+]);
